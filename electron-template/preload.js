2cb83cbdcea7ae0362f2090ded7eaddfe582f91a
Egawa Takashi <egawa.takashi@gmail.com>
2025-04-22 20:54:57 +0900

    remove templates

--- electron-template/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  loadShortcuts: () => ipcRenderer.invoke('load-shortcuts'),
-  
-  saveShortcuts: (shortcuts) => ipcRenderer.invoke('save-shortcuts', shortcuts),
-  
-  changeSaveLocation: () => ipcRenderer.invoke('change-save-location')
-});
